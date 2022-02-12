4140e5f0c35f6bb9264d687effe17bce857b4cb8
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-02-12 13:32:05 +0500

    arrow button

--- src/resources/views/welcome/index.blade.php
+++ src/resources/views/welcome/index.blade.php
@@ -95,7 +95,7 @@
         <section class="snip">
             <div class="info-block">
                 <div class="main-column-width snip__main-column sub-height main-padding-horizontal">
-                    <div class="snip__text-arrow text-arrow">
+                    <div class="snip__text-arrow text-arrow text-arrow_color_orange-2">
                         <a href="" class="text-arrow__inner text-arrow__inner_rotate">
                             <svg class="text-arrow__arrow" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M21 12L14 5M3 12L21 12L3 12ZM21 12L14 19L21 12Z" stroke="#e76100" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -136,11 +136,51 @@
                 </div>
             </div>
         </section>
-        <section class="info-block sub-height">
-            <div class="main-column-width">
-
+        <section class="mission info-block sub-height">
+            <div class="mission__main-column main-padding-horizontal main-column-width">
+                <div class="mission__text-arrow text-arrow text-arrow_color_gray">
+                    <a href="" class="text-arrow__inner text-arrow__inner_rotate">
+                        <svg class="text-arrow__arrow" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M21 12L14 5M3 12L21 12L3 12ZM21 12L14 19L21 12Z" stroke="#e76100" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <div class="text-arrow__text text_size_small text_weight_bold">наша миссия и локациях</div>
+                    </a>
+                </div>
+                <div>
+                    <div class="text text_color_black text_weight_bold text_size_desc">
+                        Свою МИССИЮ компания <br>
+                        видит в том, чтобы вдохновлять <br>
+                        людей жить. Жить за городом.
+                    </div>
+                    <div class="mission__desc text text_color_black text_weight_bold text_size_main-lg">
+                        На текущий момент в портфеле компании <a href="" class="mission__desc-link text_color_orange-1">10 проектов на разной стадии развития,</a> в том числе и полностью построенные и заселенные коттеджные поселки, в которых жители уже радуются всем преимуществам загородной жизни
+                    </div>
+                    <div class="text-arrow">
+                        <a href="" class="text-arrow__inner">
+                            <div class="text-arrow__text text_color_orange-1 text_size_main">СМОТРЕТЬ ЛОКАЦИИ</div>
+                            <svg class="text-arrow__arrow" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M21 12L14 5M3 12L21 12L3 12ZM21 12L14 19L21 12Z" stroke="#ff7a00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="sub-column-width"></div>
+            <div class="mission__sub-column sub-column-width">
+                <div class="mission__overlay"></div>
+                <div class="mission__content main-padding-horizontal">
+                    <div class="mission__sub-column-text text text_color_white text_weight_bold text_size_desc">
+                        Пожалуй, популярной локации Подмосковья — по Новорижскому шоссе, на расстоянии 7 км от Москвы. Рядом живописный лесной массив и река Липка.
+                    </div>
+                    <div class="text-arrow">
+                        <a href="" class="text-arrow__inner">
+                            <div class="text-arrow__text text_color_orange-1 text_size_main">СМОТРЕТЬ ЛОКАЦИИ</div>
+                            <svg class="text-arrow__arrow" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M21 12L14 5M3 12L21 12L3 12ZM21 12L14 19L21 12Z" stroke="#ff7a00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </section>
         <footer class="footer">
             <div class="section">
